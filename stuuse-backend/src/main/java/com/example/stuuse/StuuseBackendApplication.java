e98c9faa9ca7d50c98497686c668446037dec0a5
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-05-07 11:04:08 +0200

    Initial backend in Spring Boot

---
+++ stuuse-backend/src/main/java/com/example/stuuse/StuuseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stuuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuuseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuuseBackendApplication.class, args);
+    }
+
+}
---
+++ stuuse-backend/src/test/java/com/example/stuuse/StuuseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stuuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuuseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
