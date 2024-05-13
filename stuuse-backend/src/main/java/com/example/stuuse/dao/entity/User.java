bcf31504659ca43b3cbfc72f73b4950af72d23d1
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-05-13 22:01:24 +0200

    3. Create User entity and interface UserRepository (extending JpaRepository)

---
+++ stuuse-backend/src/main/java/com/example/stuuse/dao/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.stuuse.dao.entity;
+
+import com.example.stuuse.dao.enums.TypeOfUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "account_type")
+    private TypeOfUser accType;
+
+    @Column(name = "is_verified")
+    private boolean isVerified;
+
+}
---
+++ stuuse-backend/src/main/java/com/example/stuuse/dao/enums/TypeOfUser.java
@@ -0,0 +1,7 @@
+package com.example.stuuse.dao.enums;
+
+public enum TypeOfUser {
+    PL_EMPLOYEE,
+    THIRD_PARTY_COMPANY,
+    ADMINISTRATOR
+}
