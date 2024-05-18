f827a505a9d6115af6f9cb1721f9967d2061c1b7
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-05-18 19:15:28 +0200

    1. Add FreeHourController and UserController

---
+++ stuuse-backend/src/main/java/com/example/stuuse/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.stuuse.controller;
+
+import com.example.stuuse.dao.entity.User;
+import com.example.stuuse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(
+        path = "api/users",
+        produces = MediaType.APPLICATION_JSON_VALUE
+)
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping (consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> updateUser(@PathVariable(name = "id") Long id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteUserById(@PathVariable(name = "id") Long id) {
+        userService.deleteUserById(id);
+    }
+
+}
