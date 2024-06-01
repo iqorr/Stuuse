89a4acc43a133cb704089a6a34adb4430f2c9a72
Igor Białas <igor.bialas0@gmail.com>
2024-06-01 12:53:44 +0200

    1. Create registration page interface

--- stuuse-frontend/src/App.js
+++ stuuse-frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Home from './pages/home/Home.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Registration from './pages/registration/Registration.js';
 
 function App() {
     return (
         <Router>    
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/registration" element={<Registration />} />
             </Routes>
         </Router>
     );
