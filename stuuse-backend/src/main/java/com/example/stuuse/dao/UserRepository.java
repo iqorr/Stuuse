048d5cc18ed8e2568f6cd9fcf3ba16be911c3b0f
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-06-02 18:48:39 +0200

    4. Add endpoint to find out if the user exists

--- stuuse-backend/src/main/java/com/example/stuuse/dao/UserRepository.java
+++ stuuse-backend/src/main/java/com/example/stuuse/dao/UserRepository.java
@@ -3,8 +3,10 @@ package com.example.stuuse.dao;
 import com.example.stuuse.dao.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
+    List<User> findByLoginOrEmail(String login, String email);
 }
