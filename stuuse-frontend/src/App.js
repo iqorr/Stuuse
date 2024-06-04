ff1929e6c4a53df6e197839264808790cf47c6c2
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-06-04 22:27:56 +0200

    3. Add display of current user's data on myProfile page

--- stuuse-frontend/src/App.js
+++ stuuse-frontend/src/App.js
@@ -3,7 +3,8 @@ import Home from './pages/home/Home.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login.js';
 import Registration from './pages/registration/Registration.js';
-import Profile from './pages/profile/Porfile.js';
+import Profile from './pages/profile/Profile.js';
+import PrivateRoute from "./components/routes/PrivateRoute";
 
 
 function App() {
@@ -12,12 +13,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />d
                 <Route path="/login" element={<Login />} />
-                {/*<Route path="/myProfile" element={<PrivateRoute />}>*/}
-                {/*    <Route path="/myProfile" element={<MyProfile />} />*/}
-                {/*</Route>*/}
+                <Route path="/profile" element={<PrivateRoute />}>
+                    <Route path="/profile" element={<Profile />} />
+                </Route>
                 <Route path="/registration" element={<Registration />} />
-                <Route path="/profile" element={<Profile />} />
-           
             </Routes>
         </Router>
     );
--- stuuse-frontend/src/pages/profile/Profile.js
+++ stuuse-frontend/src/pages/profile/Profile.js
@@ -1,77 +1,123 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Profile.css'; 
 import Footer from '../../components/footer/Footer';
-import { Link, useNavigate } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import logo from '../../components/images/logo_transparent.png';
 import SectionHeader from '../../components/sectionHeader/SectionHeader';
 import Button from '../../components/button/Button';
+import Logout from "../../components/logout/Logout";
 
 
 const Profile = () => {
 
-  return (
-    <main>
-      <nav className="navbar py-3">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="Logo" width="35" height="35" className="d-inline-block align-text-top"/>
-            <span className="ms-2">Stuuse</span>
-          </Link>
-        </div>
-      </nav>
+    const [userData, setUserData] = useState({
+        name: '',
+        lastname: '',
+        login: '',
+        email: '',
+        accountType: '',
+        verified: false
+    });
 
-      <SectionHeader text="Mój profil"/>
+    const accountTypeMapping = {
+        THIRD_PARTY_COMPANY: 'Firma trzecia',
+        PL_EMPLOYEE: 'Pracownik PŁ',
+        ADMINISTRATOR: 'Administrator'
+    };
 
-      <div className='my-profile-body'>
+    useEffect(() => {
+        const userId = localStorage.getItem('userId');
+        if (!userId) {
+            console.error("No user ID found. Please log in.");
+            return; // Optionally redirect to login page
+        }
 
-            <div className='my-profile-header'>
-                {/* Get first and surname from database */}
-                <span>Jan Kowalski</span>
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/api/users/${userId}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                });
+                if (!response.ok) throw new Error('Network response was not ok');
+                const data = await response.json();
+                setUserData({
+                    name: data.name || 'N/A',
+                    lastname: data.lastname || 'N/A',
+                    login: data.login,
+                    email: data.email,
+                    accountType: accountTypeMapping[data.accType] || 'N/A',
+                    verified: data.verified ? 'TAK' : 'NIE'
+                });
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    return (
+        <main>
+          <nav className="navbar py-3">
+            <div className="container-fluid">
+              <Link className="navbar-brand" to="/">
+                <img src={logo} alt="Logo" width="35" height="35" className="d-inline-block align-text-top"/>
+                <span className="ms-2">Stuuse</span>
+              </Link>
+                <Logout />
             </div>
+          </nav>
 
-            <div className='label-name'>
-                <p>Login:</p>
-            </div>
+          <SectionHeader text="Mój profil"/>
 
-            
-            <div className='label-content mb-3'>
-                {/* Get nickname from database */}
-                <span>Jkowalski</span>
-            </div>
+          <div className='my-profile-body'>
 
-            <div className='label-name'>
-                <p>E-mail:</p>
-            </div>
+                <div className='my-profile-header'>
+                    <span>{userData.name} {userData.lastname}</span>
+                </div>
 
-            <div className='label-content mb-3'>
-                {/* Get email from database */}
-                <span>Jkowalski12@gmail.com</span>
-            </div>
+                <div className='label-name'>
+                    <p>Login:</p>
+                </div>
 
-            <div className='label-name'>
-                <p>Typ konta:</p>
-            </div>
 
-            <div className='label-content mb-3'>
-              {/* Get account type from database */}
-                <span>Frima Trzecia</span>
-            </div>
+              <div className='label-content mb-3'>
+                  <span>{userData.login}</span>
+              </div>
 
-            <div className='label-name'>
-                <p>Zweryfikowany:</p>
-            </div>
+              <div className='label-name'>
+                    <p>E-mail:</p>
+                </div>
 
-            <div className='label-content mb-3'>
-              {/* Get account verified status from database */}
-                <span>TAK</span>
-            </div>
+              <div className='label-content mb-3'>
+                  <span>{userData.email}</span>
+              </div>
 
-            <Button text="ZARZĄDZAJ"/>
-      </div>
-      
-      <Footer/>
-    </main>
-  );
+              <div className='label-name'>
+                    <p>Typ konta:</p>
+                </div>
+
+              <div className='label-content mb-3'>
+                  <span>{userData.accountType}</span>
+              </div>
+
+              <div className='label-name'>
+                    <p>Zweryfikowany:</p>
+                </div>
+
+              <div className='label-content mb-3'>
+                  <span>{userData.verified}</span>
+              </div>
+
+              <Button text="ZARZĄDZAJ"/>
+          </div>
+
+          <Footer/>
+        </main>
+    );
 }
 
 export default Profile;
