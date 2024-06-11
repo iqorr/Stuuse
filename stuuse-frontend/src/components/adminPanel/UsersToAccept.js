db2562a2ff13a43e38517330302cab939155491d
Igor Białas <igor.bialas0@gmail.com>
2024-06-11 12:09:00 +0200

    11. Handle approve and reject users in admin panel

--- stuuse-frontend/src/components/adminPanel/UsersToAccept.js
+++ stuuse-frontend/src/components/adminPanel/UsersToAccept.js
@@ -25,6 +25,63 @@ const UsersToAccept = () => {
             });
     }, []);
 
+    const handleApprove = userId => {
+        const user = users.find(u => u.userId === userId);
+        if (!user) {
+            console.error('User not found');
+            return;
+        }
+
+        const updatedData = {
+            ...user,
+            verified: true
+        };
+
+        const url = `http://localhost:8080/api/users/${userId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(updatedData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to update user status');
+                }
+                return response.json();
+            })
+            .then(() => {
+                const updatedUsers = users.map(u =>
+                    u.userId === userId ? { ...u, verified: true } : u
+                );
+                setUsers(updatedUsers);
+            })
+            .catch(error => {
+                console.error('Error updating user:', error);
+            });
+    };
+
+    const handleReject = userId => {
+        const url = `http://localhost:8080/api/users/${userId}`;
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to delete user');
+                }
+                setUsers(users.filter(user => user.userId !== userId));
+            })
+            .catch(error => {
+                console.error('Error deleting user:', error);
+            });
+    };
+
     const unverifiedUsers = users.filter(user => !user.verified);
 
     return (
@@ -34,8 +91,8 @@ const UsersToAccept = () => {
                     {user.name} {user.lastname} "{accountTypeMapping[user.accType]}" {user.email}
                 </div>
                 <div className="actions">
-                    <button className="approve">✓</button>
-                    <button className="reject">✗</button>
+                    <button className="approve" onClick={() => handleApprove(user.userId)}>✓</button>
+                    <button className="reject" onClick={() => handleReject(user.userId)}>✗</button>
                 </div>
             </div>
         ))
