420710b83dcb13627819cf3a2b178a0764981a97
Igor Białas <igor.bialas0@gmail.com>
2024-06-15 14:18:24 +0200

    15. Fix admin panel interface

--- stuuse-frontend/src/pages/administrationPanel/AdminPanel.js
+++ stuuse-frontend/src/pages/administrationPanel/AdminPanel.js
@@ -1,43 +1,49 @@
 import React from 'react';
-import Navigation from '../../components/navigation/Navigation'
 import './AdminPanel.css';
 import Footer from '../../components/footer/Footer';
 import UsersToAccept from "../../components/adminPanel/UsersToAccept";
 import ContentToAccept from "../../components/adminPanel/ContentToAccept";
 import FreeHoursToAccept from "../../components/adminPanel/FreeHoursToAccept";
+import {Link} from "react-router-dom";
+import logo from "../../components/images/logo_transparent.png";
+import Logout from "../../components/logout/Logout";
+import SectionHeader from "../../components/sectionHeader/SectionHeader";
 const AdminPanel = () =>{
     return (
         <>
-            <Navigation></Navigation>
-            <div className="admin-panel">
-                <header className="admin-header">PANEL ADMINISTRATORA</header>
-                <div className="admin-user">
-                    <h2>UŻYTKOWNICY</h2>
+            <nav className="navbar py-3">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
+                        <img src={logo} alt="Logo" width="35" height="35" className="d-inline-block align-text-top"/>
+                        <span className="ms-2">Stuuse</span>
+                    </Link>
+                    <Logout/>
                 </div>
-                <section className="admin-section">
-                    <UsersToAccept />
-                </section>
-                <div className="admin-user">
-                    <h2>WYDARZENIA/OFERTY</h2>
-                </div>
-                <section className="admin-section">
-                    <ContentToAccept />
-                </section>
-                <div className="admin-user">
-                    <h2>REKTORSKIE</h2>
-                </div>
-                <section className="admin-section">
-                    <FreeHoursToAccept typeOfFreeHour={"RECTOR_HOUR"} />
-                </section>
-                <div className="admin-user">
-                    <h2>DZIEKAŃSKIE</h2>
-                </div>
-                <section className="admin-section">
-                    <FreeHoursToAccept typeOfFreeHour={"DEAN_HOUR"} />
-                </section>
-                <Footer />
+            </nav>
 
-            </div>
+            <SectionHeader text="PANEL ADMINISTRATORA"/>
+
+            <SectionHeader text="UŻYTKOWNICY"/>
+            <section className="admin-section admin-panel-body">
+                <UsersToAccept/>
+            </section>
+
+            <SectionHeader text="WYDARZENIA/OFERTY"/>
+            <section className="admin-section admin-panel-body">
+                <ContentToAccept/>
+            </section>
+
+            <SectionHeader text="REKTORSKIE"/>
+            <section className="admin-section admin-panel-body">
+                <FreeHoursToAccept typeOfFreeHour={"RECTOR_HOUR"}/>
+            </section>
+
+            <SectionHeader text="DZIEKAŃSKIE"/>
+            <section className="admin-section admin-panel-body">
+                <FreeHoursToAccept typeOfFreeHour={"DEAN_HOUR"}/>
+            </section>
+
+            <Footer/>
         </>
 
     );
